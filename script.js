7cc65a2575671991fb618a329a41b97cf22ec933
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-27 22:24:00 +0530

    grabbing elements in js

---
+++ script.js
@@ -0,0 +1,6 @@
+const hourEl = document.querySelector('.hour')
+const minuteEl = document.querySelector('.minute')
+const secondEl = document.querySelector('.second')
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+const toggle = document.querySelector('.toggle')
