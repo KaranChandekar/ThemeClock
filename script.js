75e752d314446a6fa4830d4e7d3691a46002a5a6
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-27 22:25:35 +0530

    initialize days and months

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const secondEl = document.querySelector('.second')
 const timeEl = document.querySelector('.time')
 const dateEl = document.querySelector('.date')
 const toggle = document.querySelector('.toggle')
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
